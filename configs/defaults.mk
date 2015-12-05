6089d5c31be4fb2d2c11f54043e9d0f4a8c37a9f
Thibault Kruse <thibault.kruse@gmx.de>
2015-12-05 22:18:13 +0100

    bazel 0.1.1 with fixed bazelrc

--- configs/defaults.mk
+++ configs/defaults.mk
@@ -34,7 +34,7 @@ export LEININGEN_DEFAULT_VERSION=2.5.3
 export SBT_DEFAULT_VERSION=0.13.9
 # pants is horrible at backwards compatibility, expect problems when upgrading
 export PANTS_DEFAULT_VERSION=0.0.63
-export BAZEL_DEFAULT_VERSION=0.1.2
+export BAZEL_DEFAULT_VERSION=0.1.1
 
 ## Build orchestration variables
 ## Should usually be left alone
