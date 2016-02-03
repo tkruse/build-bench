ce686cbd89574ba20654aa5c9333a99456e6de8c
Thibault Kruse <thibault.kruse@gmx.de>
2016-02-04 00:50:28 +0100

    raise number of subprojects to keep 8 cpus busy

--- configs/generated_multi.mk
+++ configs/generated_multi.mk
@@ -3,8 +3,8 @@ export BUILD_DEFINITIONS=multiModule
 # Definition of source files
 export SOURCE_PROJECT=multi
 # Configuration for template-based sources
-export FILE_NUM=20
-export SUBPROJECT_NUM=8
+export FILE_NUM=10
+export SUBPROJECT_NUM=16
 # Name of the benchmark, used for folder and report
 export TARGET_NAME=$(SOURCE_PROJECT)-$(SUBPROJECT_NUM)-$(FILE_NUM)
 
